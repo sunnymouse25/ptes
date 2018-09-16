1afe0ef34ac385a0f3d4a9c84bddf81e95bd13b2
sunnymouse25 <sunnymouse25@gmail.com>
2018-09-16 16:28:27 +0300

    fixed imports

--- ptes/ptes.py
+++ ptes/ptes.py
@@ -1,40 +1,41 @@
 from collections import defaultdict, OrderedDict
 from interval import interval
-import pandas as pd
 import numpy as np
 
-def order_cigar(row):   
-    '''
+
+def order_cigar(row):
+    """
     Returns pairs attribute - number in the order as in CIGAR: (['S1', 'M1'], [44, 32])
-    '''
+    """
     attr = []
     nums = []
     num = ''
-    index_dict = defaultdict(lambda:1) 
+    index_dict = defaultdict(lambda: 1)
     for letter in row:
         if letter in '-0123456789':
             num += letter
         if letter in 'MDINSpH':
-            attr.append(letter+str(index_dict[letter]))
-            index_dict[letter] +=1
+            attr.append(letter + str(index_dict[letter]))
+            index_dict[letter] += 1
             nums.append(int(num))
             num = ''
-    return attr,nums  
-    
-def get_read_interval(cigar, leftpos, output='dict'):    
+    return attr, nums
+
+
+def get_read_interval(cigar, leftpos, output='dict'):
     features, lengths = order_cigar(cigar)
     if isinstance(leftpos, str):
         leftpos = int(leftpos)
-    cigar_dict = OrderedDict(zip(features, lengths)) # for match_interval
-    read_interval = interval()    
+    cigar_dict = OrderedDict(zip(features, lengths))  # for match_interval
+    read_interval = interval()
     read_list = []
-    i = 0    # counting features
+    i = 0  # counting features
     indels = []
-    i_to_del = False    
-    for feature, length in cigar_dict.items():     # OrderedDict is important here
+    i_to_del = False
+    for feature, length in cigar_dict.items():  # OrderedDict is important here
         if i == 0 and (feature == 'S1' or feature == 'H1'):  # soft-clip in the beginning is not included in leftpos
             k = interval[leftpos - length, leftpos - 1]
-        elif 'I' in feature:    # does not consume reference
+        elif 'I' in feature:  # does not consume reference
             indels.append(feature)
             i_to_del = True
             continue
@@ -43,36 +44,38 @@ def get_read_interval(cigar, leftpos, output='dict'):
             leftpos = leftpos + length
         read_interval = read_interval | k
         read_list.append(k)
-        i += 1    
-        
-    if i_to_del:    # indels do not have genomic intervals
-        for x in indels:    # we need to delete them to achieve the same length of features and intervals
+        i += 1
+
+    if i_to_del:  # indels do not have genomic intervals
+        for x in indels:  # we need to delete them to achieve the same length of features and intervals
             del cigar_dict[x]
-    read_dict = OrderedDict(zip(cigar_dict.keys(), read_list))   #{'S1': interval([586.0, 657.0]), 'M1': interval([658.0, 686.0])}        
+    read_dict = OrderedDict(zip(cigar_dict.keys(),
+                                read_list))  # {'S1': interval([586.0, 657.0]), 'M1': interval([658.0, 686.0])}
     if output == 'interval':
         return read_interval
-    elif output == 'dict': 
-        return read_dict    
+    elif output == 'dict':
+        return read_dict
+
 
 def read_sam_attrs(sam_name, tag_list):
-    '''
+    """
     Reads SAM file (no header), looks for tags in extra fields
     Example tag_list = ["NH", "HI", "XO","XS","XT"]
     Returns list ready for DataFrame:
     df_sam = pd.DataFrame(csv_list)
-    '''
-    csv_list = []    
+    """
+    csv_list = []
     with open(sam_name, 'r') as sam_file:
         for line in sam_file:
-            row = line.strip().split('\t') 
+            row = line.strip().split('\t')
             flag = row[1]
             if flag == 4:
-                continue   # unmapped
-            sam_attrs = {'read_name' : row[0],
-                              'flag': flag,                              
-                              'chrom' : row[2], 
-                              'leftpos' : row[3],
-                              'cigar' : row[5]}
+                continue  # unmapped
+            sam_attrs = {'read_name': row[0],
+                         'flag': flag,
+                         'chrom': row[2],
+                         'leftpos': row[3],
+                         'cigar': row[5]}
             tags = dict.fromkeys(tag_list, np.nan)
             for elm in row[11:]:
                 for tag in tag_list:
@@ -81,54 +84,57 @@ def read_sam_attrs(sam_name, tag_list):
             sam_attrs.update(tags)
             csv_list.append(sam_attrs)
     return csv_list
-    
+
+
 def one_interval(I):
-    '''
+    """
     Returns one large interval from set of intervals
     with extreme points of I
-    '''
+    """
     return interval[I[0].inf, I[-1].sup]
-    
-def get_interval_length(i):   #i is interval object of any length
+
+
+def get_interval_length(i):  # i is interval object of any length
     interval_length = 0
     for x in i.components:
         if len(x) > 0: interval_length += (x[0].sup - x[0].inf + 1)
     return int(interval_length)
-    
-def len_to_coord(len_list):   # from lengths of exons to the list of intervals      
+
+
+def len_to_coord(len_list):  # from lengths of exons to the list of intervals
     intervals = []
-    coord = 0                 # 0-based position
-    for length in len_list:         
+    coord = 0  # 0-based position
+    for length in len_list:
         k = interval[coord, coord + length - 1]
         coord = coord + length
         intervals.append(k)
-    return intervals   
+    return intervals
+
 
 def actual_exon_numbers(exons):
-        '''
-        Takes a string from transcript name
-        Returns list of actual exon numbers in transcript
-        '''
-        if len(exons) < 4 or len(exons) > 12:
-            return [0, 0, 0, 0]
-        if len(exons) == 4: 
-            exon_list = list(exons)            
-        if len(exons) == 5:
-            exon_list = map(str, [7,9,8,10])                        
-        if len(exons) == 6:
-            exon_list = map(str, [8,10,9,11])
-        if len(exons) == 7:
-            exon_list = map(str, [9,11,10,12])
-        if len(exons) == 8:
-            exon_list = [exons[0:2],exons[2:4],exons[4:6],exons[6:8]]
-        if len(exons) == 9:
-            exon_list = map(str, [97,99,98,100])                        
-        if len(exons) == 10:
-            exon_list = map(str, [98,100,99,101])                        
-        if len(exons) == 11:
-            exon_list = map(str, [99,101,100,102])
-        if len(exons) == 12:
-            exon_list = [exons[0:3],exons[3:6],exons[6:9],exons[9:12]]
-        return exon_list
-
-        
+    """
+    Takes a string from transcript name
+    Returns list of actual exon numbers in transcript
+    """
+    exon_list = list()
+    if len(exons) < 4 or len(exons) > 12:
+        return [0, 0, 0, 0]
+    if len(exons) == 4:
+        exon_list = list(exons)
+    if len(exons) == 5:
+        exon_list = map(str, [7, 9, 8, 10])
+    if len(exons) == 6:
+        exon_list = map(str, [8, 10, 9, 11])
+    if len(exons) == 7:
+        exon_list = map(str, [9, 11, 10, 12])
+    if len(exons) == 8:
+        exon_list = [exons[0:2], exons[2:4], exons[4:6], exons[6:8]]
+    if len(exons) == 9:
+        exon_list = map(str, [97, 99, 98, 100])
+    if len(exons) == 10:
+        exon_list = map(str, [98, 100, 99, 101])
+    if len(exons) == 11:
+        exon_list = map(str, [99, 101, 100, 102])
+    if len(exons) == 12:
+        exon_list = [exons[0:3], exons[3:6], exons[6:9], exons[9:12]]
+    return exon_list
--- ptes/ucsc/ucsc.py
+++ ptes/ucsc/ucsc.py
@@ -1,35 +1,39 @@
 from collections import OrderedDict, defaultdict
-from interval import interval
+
+from ptes.ptes import get_interval_length
+
 
 def list_to_dict(lst):
-    '''
+    """
     From list of intervals makes dict of M's
     {'M1': [interval([70799946.0, 70799962.0], 'M2': interval([70801644.0, 70801667.0])}
-    '''
+    """
     numbers = range(1, len(lst)+1)
     names = map(lambda x: 'M' + str(x), numbers)
     dct = OrderedDict(zip(names,lst))
     return dct
 
+
 def order_interval_list(values):
-    '''
+    """
     For list of intervals returns list in increasing order:
     inverts for '-' chain
-    '''  
+    """
     start = values[0][0].inf
     last_start = values[-1][0].inf  
     if start > last_start:
         values = values[::-1]
     return values
 
+
 def get_track_list(chrom, chain, read_dict, name = 'sim_read', color = 'r'):
-    '''
+    """
     Gets chrom, chain, dictionary of read intervals instead of cigar and leftpos;
     Example for read_dict: {'S1': interval([586.0, 657.0]), 'M1': interval([658.0, 686.0])} 
     Name may be CIGAR string or read_name;
     Color: red by default, may be 'b', 'g' or in RGB code like '255,0,255'
     Returns .bed line with all tracks in the list
-    '''
+    """
     track_list = [chrom]    
     blockSizes = []
     chromStarts = []
@@ -51,13 +55,13 @@ def get_track_list(chrom, chain, read_dict, name = 'sim_read', color = 'r'):
         if i == len(read_dict)-1:
             chromEnd = int(value[0].sup)
         i+=1    
-    track_list.append(chromStart) #chromStart, counts from 0
-    track_list.append(chromEnd)   #chromEnd, not included
-    track_list.append(name)      #name
-    track_list.append('0')        #score
-    track_list.append(chain)      #chain
-    track_list.append(thickStart) #thickStart
-    track_list.append(thickEnd)   #thickEnd
+    track_list.append(chromStart) # chromStart, counts from 0
+    track_list.append(chromEnd)   # chromEnd, not included
+    track_list.append(name)      # name
+    track_list.append('0')        # score
+    track_list.append(chain)      # chain
+    track_list.append(thickStart) # thickStart
+    track_list.append(thickEnd)   # thickEnd
     if color == 'r':
         track_list.append('255,0,0')  #color
     elif color == 'g':
@@ -65,8 +69,8 @@ def get_track_list(chrom, chain, read_dict, name = 'sim_read', color = 'r'):
     elif color == 'b':
         track_list.append('0,0,255') 
     else:
-        track_list.append(color)        #fuchsia is '255,0,255'
-    track_list.append(len(blockSizes))        #blockCount is length of blockSizes
-    track_list.append(','.join(map(str,blockSizes)))         #blockSizes
-    track_list.append(','.join(map(str,chromStarts)))        #chromStarts
+        track_list.append(color)        # fuchsia is '255,0,255'
+    track_list.append(len(blockSizes))        # blockCount is length of blockSizes
+    track_list.append(','.join(map(str,blockSizes)))         # blockSizes
+    track_list.append(','.join(map(str,chromStarts)))        # chromStarts
     return map(str, track_list)
