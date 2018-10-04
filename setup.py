380fdcdab0e0f5f25d352b1500e390973cd2e47a
sunnymouse25 <sunnymouse25@gmail.com>
2018-10-04 21:46:11 +0300

    logger bugfix

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ setup(
     name='ptes',
     version='0.1',
     packages=find_packages(),
-    package_data={},
+    package_data={'ptes': ['configs/*', 'tests/*']},
     install_requires=[
         'pandas>=0.23.4',
         'numpy>=1.15.1',
-        'pyinterval>=1.2.0'
+        'pyinterval>=1.2.0',
+        'pyaml'
         ]
     )
